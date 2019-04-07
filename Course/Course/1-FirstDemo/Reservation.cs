cbcdd6a2e1fae3b7346849b8572580f2095aac48
fabio.tavoloni <fabio.tavoloni@LNLT-PC0YUUXY>
2019-04-07 17:52:06 +0100

    first demo class

---
+++ Course/Course/1-FirstDemo/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// The objective of this class is describe the first scenarios that we can map inside a method
+/// </summary>
+namespace Course._1_FirstDemo
+{
+    class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (MadeBy == user)
+                return true;
+
+            return false;
+        }
+    }
+
+    class User
+    {
+        public bool IsAdmin { get; internal set; }
+    }
+}
